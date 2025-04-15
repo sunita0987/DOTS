8299ac0ba74d0decd1770d7195ea17342711f7fd
sunita0987 <sunita0987546@gmail.com>
2025-04-15 14:42:56 +0530

    program add

---
+++ assets/js/program.js
@@ -0,0 +1,45 @@
+// 1. WAP to add three numbers using function
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+    console.log(addThreeNumbers(10, 20, 30));
+
+//2. WAP to find the leap year using a function
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isLeapYear(2004)); 
+
+//3.check if a string is palindrome 
+function isPalindrome(str) {
+    const reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+}
+console.log(isPalindrome("madam"));
+
+//4. WAP to find the factorial of a number using function
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5)); 
+
+//5.find the largest number in an array using function
+function findLargestNumber(arr) {
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+console.log(findLargestNumber([12,45,78,23,89,34]));
+
